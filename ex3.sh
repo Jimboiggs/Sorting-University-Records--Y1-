44953a3ff7e4ff100c47d4c68dda12f59a537857
James Worsley <jamesworsley07@gmail.com>
2025-12-03 00:17:31 +0000

    Fixed ex3.sh

--- ex3.sh
+++ ex3.sh
@@ -3,10 +3,10 @@ echo ".csv file path: $1"
 echo "Named header: $2"
 echo "Number of rows: $3"
 
-if [ ! head -n 1 "$1" | grep -qw "$2" ]; then
+if ! head -n 1 "$1" | grep -qw "$2"; then
 	echo "$2 not a header in $1"
 
-elif [ $3 = "" ];
+elif [ $3 = "" ]; then
 	col=$(head -n 1 "$1" | tr ',' '\n' | grep -nw "$2" | cut -d: -f1)
 	cat $1 | sort -t ',' -k"$col","$col" -r "$1"
 
