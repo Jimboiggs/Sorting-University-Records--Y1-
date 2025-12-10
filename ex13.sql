0997ae0c72a9061eb3d287b5906a73e4bad5c4e1
Jimboiggs <jamesworsley07@gmail.com>
2025-12-10 16:26:01 +0000

    Fixed JOIN syntax

--- ex13.sql
+++ ex13.sql
@@ -1,5 +1,5 @@
 SELECT studentID, student_firstname, student_surname, exam_mark
 FROM STUDENTS
-RIGHT JOIN STUDENTMODULES ON studentID
+RIGHT JOIN STUDENTMODULES ON STUDENTS.studentID = STUDENTMODULES.studentID
 WHERE year = 1 AND moduleID LIKE '%COMP%'
 ORDER BY mark DESC;
